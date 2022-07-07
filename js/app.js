369f860814bfac095b18af866d2939dc7a5e6924
liemle417 <47941056+liemle417@users.noreply.github.com>
2022-07-07 01:36:24 -0700

    Update Files

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-const traffic_chart = document.getElementById('traffic_chart').getContext('2d');
+let trafficChart = document.getElementById('trafficChart').getContext('2d');
 
-const new_traffic_chart = new Chart(traffic_chart, {
+let new_traffic_chart = new Chart(trafficChart, {
     type: 'line',
     data: {
-        x: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-        y: ['0', '500', '1000', '1500', '2000', '2500'],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            data: [0, 750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+
+            backgroundColor: 'rgb(45, 47, 75, 0.2)',
+            fill: 'rgb(45, 47, 75, 0.2)',
+            borderColor: 'rgb(92, 27, 152)',
+            borderWidth: 2,
+            pointRadius: 5,
+            pointBackgroundColor: 'white',
+        }],
+    },
+
+    options: {
+        plugins:{
+            legend:{
+                display: false
+            }
+        },
+
+        
     }
-})
+});
